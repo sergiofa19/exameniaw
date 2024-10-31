9db2e9f7cf47a9608ad85b75123e200f4478f1ff
Sergio Fernández Avila <fefsd@gmailcom>
2024-10-31 08:56:48 +0000

    segundo commit

---
+++ examen.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>sergio</h1>
+   <h2>31/10/2024</h2> 
+   <h3>este es un repositorio git</h3>
+</body>
+</html>
